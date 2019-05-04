8eee399975cae09986c08d815c204cad9165d33d
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-05-04 23:18:17 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,6 +83,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-127901499-3",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
