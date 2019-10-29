e7bd10a173e1ddcab654ae57218286947b8cab8d
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-10-29 17:51:46 +0100

    fix unvote mutation

--- functions/db/db.js
+++ functions/db/db.js
@@ -32,7 +32,7 @@ exports.handler = async function(event, context) {
   try {
     const { query, variables } = JSON.parse(body)
     if (query.includes("deleteEpisode") || query.includes("updateEpisode")) {
-      const msg = `operation not allowed `
+      const msg = `delete operation not allowed `
       return {
         statusCode: 500,
         body: JSON.stringify({ msg }),
@@ -40,7 +40,12 @@ exports.handler = async function(event, context) {
     }
     if (query.includes("mutation")) {
       const user = jwtDecode(authorization)
-      if (!!variables.data.email && user.email !== variables.data.email) {
+      console.log(user, variables)
+      if (
+        !!variables.data &&
+        !!variables.data.email &&
+        user.email !== variables.data.email
+      ) {
         console.log(variables, user)
         const msg = `operation not allowed `
         return {
