73ecdee92344bdfbfc147ebf36767893b8138747
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-12-29 17:02:21 +0100

    create Hero component

---
+++ src/components/Home/Hero.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import PlayIcon from "assets/play.svg"
+
+export default () => {
+  return (
+    <div className="container intro">
+      <h1> Tech Topics In Darija </h1>
+      <p>
+        We are Developer Circles Casablanca community, we believe in community
+        and sharing knowladge
+      </p>
+      <div className="actions">
+        <Link to="/blablas" className="button">
+          <PlayIcon /> Start Watching
+        </Link>
+        <Link to="/blablas" className="button outline">
+          Suggest a topic
+        </Link>
+      </div>
+    </div>
+  )
+}
