1aa6cecb4006c031a96a5344ec1baeef0bfb6c8b
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-12-29 19:48:38 +0100

    review component

---
+++ src/components/Home/Reviews.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Pattern from "assets/pattern.svg"
+
+export default () => {
+  return (
+    <div className="reviews">
+      <Pattern width="100%" />
+      <h1> Reviews </h1>
+    </div>
+  )
+}
