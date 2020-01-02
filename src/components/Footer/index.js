4221f3ac15c64425aeae5fc086757ef42b6cc036
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-01-02 20:08:30 +0100

    add icon to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,28 @@
 import React from "react"
 import "./index.scss"
+
+import Auth0 from "assets/auth0.svg"
+import GatsbyJs from "assets/gatsby.svg"
+import ReactIcon from "assets/react-icon.svg"
+import Figma from "assets/figma.svg"
+import Netlify from "assets/netlify.svg"
+import Github from "assets/github.svg"
+
 export default ({}) => (
   <div className="footer">
     <div className="container">
-      <p>Want to Contribute?</p>
-      <p>© DevC Casablanca 2019 </p>
-      <p> This WebsiteMade with ❤️ By DevC Team</p>
       <p>
-        Using React, Gatsby.js, FunaDB and Auth0, designed using Figma, hosted
-        on Netlify. The source is on GitHub.
+        <a href="https://github.com/DevC-Casa/geeksblabla.com">
+          Want to Contribute?
+        </a>
+      </p>
+      <p>© DevC Casablanca 2019 </p>
+      <p>
+        This Website Made with ❤️ By <a> DevC Team </a>
+      </p>
+      <p>
+        <ReactIcon alt="react" /> <GatsbyJs /> FunaDB <Auth0 />
+        <Figma /> <Netlify /> <Github />
       </p>
     </div>
   </div>
