9579ad1fa8c40e671d8c9b8979538049828224b1
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-01-04 18:29:57 +0100

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,13 @@ import React from "react"
 
 import Layout from "components/Layout"
 import SEO from "components/SEO"
-import { Hero, Contributors } from "components/About"
+import { Hero, Contributors, ContactUs } from "components/About"
 
 export default () => (
   <Layout>
     <SEO />
     <Hero />
     <Contributors />
+    <ContactUs />
   </Layout>
 )
