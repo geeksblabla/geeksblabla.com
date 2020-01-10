b6237eaff023807596b66a0de01b886a91fe7806
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-01-10 16:51:50 +0100

    fix context error on build

--- src/components/Mode/ModeContext.js
+++ src/components/Mode/ModeContext.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 const isBrowser = typeof window !== "undefined"
 
-export const ModeContext = React.createContext()
+export const ModeContext = React.createContext({ isVideo: false })
 
 export const ModeProvider = ({ children }) => {
   const [isVideo, setIsVideo] = useState(
