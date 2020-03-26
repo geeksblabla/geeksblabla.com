b0f57b6d26d0fd70d1ba1386ee0e407b7ef36bb0
Mourad Aouinat <mourad.eyes@gmail.com>
2020-03-26 07:00:34 +0100

    create custume P tag that detects timestamps

--- src/components/mdx/index.js
+++ src/components/mdx/index.js
@@ -5,4 +5,27 @@ export default {
   h1: ({ children, ...props }) => <h4 {...props}>{children}</h4>,
   h2: ({ children, ...props }) => <h4 {...props}>{children}</h4>,
   h3: ({ children, ...props }) => <h4 {...props}>{children}</h4>,
+  p: ({ children, ...props }) => {
+    const regex = /[0-9]{1,2}:[0-9]{1,2}/
+
+    let time, seconds, text
+
+    if (typeof children === "string") {
+      time = children.match(regex)
+
+      if (time) {
+        let timeArray = time[0].split(":")
+        seconds =
+          parseInt(timeArray[0], 10) * 3600 + parseInt(timeArray[1], 10) * 60
+        text = children.slice(time[0].length, children.length)
+      }
+    }
+
+    return (
+      <p {...props}>
+        {time && <a href={`#${seconds}`}>{time}</a>}
+        {text ? text : children}
+      </p>
+    )
+  },
 }
