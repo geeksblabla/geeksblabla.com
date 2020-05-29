4bdbbddcc6fa23ca7ed0e41f22dc3b0a5875aa4d
Mohamed El Bahja <mohamed.jss11@gmail.Com>
2020-05-29 20:59:38 +0100

    Add Gatsby Sitemap. (#136)
    * add: gatsby sitemap
    
    * add: robots file.

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,5 +118,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: ["/thanks/"],
+      },
+    },
   ],
 }
