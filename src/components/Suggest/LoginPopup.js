30cf9a30e45419263cef73bd55b17855a55ddb55
zakariasaad <zakaria.elas@gmail.com>
2020-07-26 14:28:23 +0100

    fix: contrast in login popup using light theme (#157)

--- src/components/Suggest/LoginPopup.js
+++ src/components/Suggest/LoginPopup.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Popup from "reactjs-popup"
 import { Auth0Context } from "./auth0"
+import { useTheme } from "components/Theme/ThemeContext"
 
 export default React.forwardRef((props, ref) => {
   const { login } = React.useContext(Auth0Context)
+  const { dark } = useTheme()
 
   return (
     <Popup
       ref={ref}
-      contentStyle={contentStyle}
+      contentStyle={getContentStyle(dark)}
       overlayStyle={{ backgroundColor: "rgba(25, 26, 68, 0.68)" }}
     >
       <div className="login-popup">
@@ -29,9 +31,9 @@ export default React.forwardRef((props, ref) => {
   )
 })
 
-const contentStyle = {
-  backgroundColor: "#121334",
-  border: `1px solid #7577a6`,
+const getContentStyle = (dark) => ({
+  backgroundColor: dark ? "#121334" : "#fff",
+  border: dark ? `1px solid #7577a6` : `1px solid #31337e`,
   padding: `0px`,
   width: "auto",
-}
+})
