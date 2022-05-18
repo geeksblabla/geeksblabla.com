20a384da3c2976809578b24eeceda913fe64397a
Corentin Deleuse <59571508+deleusec@users.noreply.github.com>
2022-05-18 17:19:16 +0200

    update footer style (#329)
    * update footer style
    
    * add linkedin + fixes
    
    Co-authored-by: Youssouf EL Azizi <youssoufelazizi@gmail.com>

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,43 +7,136 @@ import ReactIcon from "assets/react-icon.svg"
 import Figma from "assets/figma.svg"
 import Netlify from "assets/netlify.svg"
 import FunaDB from "assets/funadb.svg"
+import { Link } from "gatsby"
+import Logo from "../../assets/logo.svg"
 
 export default ({ style }) => (
-  <div className="footer" style={style}>
-    <div className="container">
-      <p>
-        <a href="https://github.com/DevC-Casa/geeksblabla.com" rel="noopener">
-          Want to Contribute?
-        </a>
-      </p>
+  <div className="Footer" style={style}>
+    <div className="Footer-container">
+      <div className={"Footer-main"}>
+        <div className={"Footer-main-logo"}>
+          <Link to="/">
+            <Logo alt="Logo" />
+          </Link>
+        </div>
 
-      <p>
-        This Website is made with{" "}
-        <span role="img" aria-label="love">
-          ❤️
-        </span>{" "}
-        By{" "}
-        <a
-          href="https://www.facebook.com/groups/DevC.Casablanca/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          DevC Casa Team{" "}
-        </a>{" "}
-        using
-      </p>
-      <p>
-        <Figma /> <ReactIcon alt="react" /> <GatsbyJs /> <FunaDB /> <Auth0 />
-        <Netlify />
-      </p>
-      <p>
-        Sourced on{" "}
-        <a href="https://github.com/DevC-Casa/geeksblabla.com" rel="noopener">
-          GitHub
-        </a>
-      </p>
-      <p>© DevC Casablanca 2018 - {new Date().getFullYear()}</p>
+        <div className={"Footer-main-links"}>
+          <div className={"Footer-main-follow"}>
+            <p>Follow</p>
+            <ul>
+              <li>
+                <a
+                  target={"_blank"}
+                  href={"https://www.youtube.com/c/GeeksBlaBla01"}
+                >
+                  Youtube
+                </a>
+              </li>
+              <li>
+                <a target={"_blank"} href={"https://twitter.com/geeksblabla"}>
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href={"https://www.linkedin.com/company/69317726/"}
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href={"https://www.facebook.com/geeksblabla/"}
+                >
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href={"https://www.instagram.com/geeksblabla/?hl=fr"}
+                >
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className={"Footer-main-community"}>
+            <p>More</p>
+            <ul>
+              <li>
+                <a target={"_blank"} href="https://www.stateofdev.ma">
+                  StateOfDev.ma
+                </a>
+              </li>
+              <li>
+                <a target={"_blank"} href="https://www.blablaconf.com">
+                  BlablaConf.com
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href="https://github.com/DevC-Casa/awesome-morocco"
+                >
+                  awesome-morocco
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={"Footer-main-contribution"}>
+          <p>
+            <Link
+              to="https://github.com/DevC-Casa/geeksblabla.com"
+              rel="noopener"
+            >
+              Want to Contribute?
+            </Link>
+          </p>
+
+          <p>
+            This Website is made with{" "}
+            <span role="img" aria-label="love">
+              ❤️
+            </span>{" "}
+          </p>
+          <p>
+            By{" "}
+            <a
+              href="https://www.facebook.com/groups/DevC.Casablanca/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              DevC Casa Team{" "}
+            </a>{" "}
+            using
+          </p>
+          <p>
+            <Figma /> <ReactIcon alt="react" /> <GatsbyJs /> <FunaDB />{" "}
+            <Auth0 />
+            <Netlify />
+          </p>
+          <p>
+            Sourced on{" "}
+            <Link
+              to="https://github.com/DevC-Casa/geeksblabla.com"
+              rel="noopener"
+            >
+              GitHub
+            </Link>
+          </p>
+        </div>
+      </div>
+
+      <div className={"Footer-copyright"}>
+        <p>© Geeksblabla 2018 - {new Date().getFullYear()}</p>
+      </div>
     </div>
   </div>
 )
