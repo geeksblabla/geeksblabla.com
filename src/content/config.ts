2ac14a571e8c510fea317a102f78e7de9fe6078e
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-10-18 16:30:48 +0100

    feat: mock cloudinary loader to allow people to use website without cloudinary keys

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,32 @@
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { cldAssetsLoader } from "astro-cloudinary/loaders";
 import { episodeSchema } from "./shema";
-
+/**
+ * Podcast collection
+ * Read episodes markdown files from episodes folder in the root of the project
+ * Validate the data using zod
+ * Return the data based on the episodeSchema
+ */
 const podcast = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: "episodes" }),
   schema: episodeSchema,
 });
 
-const gallery = defineCollection({
-  loader: cldAssetsLoader({
-    limit: 10,
-    folder: "community-gallery",
-  }),
-});
+/**
+ * Gallery collection
+ * As you need env vars to use cldAssetsLoader, we need to use a mock data to allow people to play with the website locally
+ */
+
+const gallery = !import.meta.env.PUBLIC_CLOUDINARY_CLOUD_NAME
+  ? defineCollection({
+      loader: file("gallery-mock-data.json"),
+    })
+  : defineCollection({
+      loader: cldAssetsLoader({
+        limit: 10,
+        folder: "community-gallery",
+      }),
+    });
 
 export const collections = { podcast, gallery };
